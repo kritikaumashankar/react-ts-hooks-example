4a4bf8eb4d3c1fd52877468766579b4a484753ce
Shiv Kumar Subramaniam <shivsubramaniam@shivs-mac-mini.lan>
2023-03-31 10:29:01 -0600

    react typescript hooks example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import { useState, useEffect, useCallback, useMemo, useRef, MouseEvent, KeyboardEvent} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface User {
+  id: number,
+  username: string,
+}
+
+type fibFunc=(n: number) => number;
+
+const fib : fibFunc =(n) => {
+  if(n<2) return n;
+
+  return fib(n-1) + fib(n-2);
+}
+
+const myNum: number = 37;
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  //
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
+
+  // handles side effects -> mount -> unmount -> remount
+  //when something changes something else could happen
+  useEffect(() => {
+    console.log('mounting')
+    console.log('Users: ',users)
+  }, [users])
+
+  /*
+  useCallback - memoize while event
+   */
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>) => setCount(prev => prev+1), []);
+
+  //useMemo - memoizes the value.
+  const result = useMemo(() => fib(myNum),[myNum]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text" />
     </div>
   );
 }
